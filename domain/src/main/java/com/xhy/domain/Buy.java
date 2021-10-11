09e4fed6707dd33572beeebfcb110b0123306273
meksuper <71376153+meksuper@users.noreply.github.com>
2021-10-11 13:27:06 +0800

    提交代码修改

--- domain/src/main/java/com/xhy/domain/Buy.java
+++ domain/src/main/java/com/xhy/domain/Buy.java
@@ -1,13 +1,17 @@
 package com.xhy.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
+
 @Data
 public class Buy implements Serializable {
     private int buyid;
     private String buytitle;
-    private String btime;
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date btime;
     private int bdaytype;
     private String itemtype;
     private String itemid;
